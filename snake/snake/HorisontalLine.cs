0e150b0fd1e60300ede32f373b69c3bdd3e93865
ArtyBaykov <artembaykov76@gmail.com>
2018-04-12 01:37:19 +0300

    Класс для горизонтальный линий

---
+++ snake/snake/HorisontalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace snake
+{
+    class HorisontalLine
+    {
+        List<Point> pList;
+
+        public HorisontalLine(int xRight, int xLeft, int y, char sym)
+        {
+            pList = new List<Point>();
+            for(int x = xRight; x <= xLeft; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+
+        }
+
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
